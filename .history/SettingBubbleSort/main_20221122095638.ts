824152b05ba190fb6578603c8e1939cfc2918bbd
hungnguyen50497 <nguyenhung50497@gmail.com>
2022-11-22 09:58:50 +0700

    new

---
+++ .history/SettingBubbleSort/main_20221122095638.ts
@@ -0,0 +1,3 @@
+export class BubbleSort {
+    static list: number[] = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12];
+}
---
+++ .history/SettingBubbleSort/main_20221122095645.ts
@@ -0,0 +1,4 @@
+export class BubbleSort {
+    static list: number[] = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12];
+    
+}
